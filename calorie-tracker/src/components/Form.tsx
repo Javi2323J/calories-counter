e1c0e3e5016ba09541a70993c5510946d1d06499
DESKTOP-C6NF0QP\jrome <jromera232003@gmail.com>
2025-01-23 13:42:34 +0100

    work on Form component WIP #1

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,13 +1,39 @@
-import React from 'react'
+import React, { useState, ChangeEvent, FormEvent } from 'react'
 
+import { Activity } from '../types'
 import { categories } from '../data/categories'
 
 export default function Form(): React.JSX.Element {
+    const [activity, setActivity] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id)
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity
+
+        return name.trim() !== '' && calories > 0
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+    }
+
     return (
-        <form className='space-y-5 bg-white shadow p-10 rounded-lg'>
+        <form onSubmit={handleSubmit} className='space-y-5 bg-white shadow p-10 rounded-lg'>
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="category" className="font-bold">Category:</label>
-                <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+                <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category" value={activity.category} onChange={handleChange}>
                     {categories.map(category => (
                         <option key={category.id} value={category.id}>
                             {category.name}
@@ -17,16 +43,16 @@ export default function Form(): React.JSX.Element {
             </div>
 
             <div className="grid grid-cols-1 gap-3">
-                <label htmlFor="excercise" className="font-bold">Excercise:</label>
-                <input id="excercise" type="text" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Food" />
+                <label htmlFor="name" className="font-bold">Activity:</label>
+                <input id="name" type="text" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Food" value={activity.name} onChange={handleChange} />
             </div>
 
             <div className="grid grid-cols-1 gap-3">
                 <label htmlFor="calories" className="font-bold">Calories:</label>
-                <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg" placeholder="Calories" />
+                <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg" placeholder="Calories" value={activity.calories} onChange={handleChange} />
             </div>
 
-            <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value="Save food or excercise" />
+            <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10" value={activity.category === 1 ? 'Save Food' : 'Save Activity'} disabled={!isValidActivity()} />
         </form>
     )
 }
