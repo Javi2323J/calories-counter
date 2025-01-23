1fdf028f58f5286019cef5f8f56d3df6fb2f9d7d
DESKTOP-C6NF0QP\jrome <jromera232003@gmail.com>
2025-01-23 13:18:58 +0100

    add types foldes; define Form component #1

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
+import { categories } from '../data/categories'
+
 export default function Form(): React.JSX.Element {
     return (
-        <div>Form</div>
+        <form className='space-y-5 bg-white shadow p-10 rounded-lg'>
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="category" className="font-bold">Category:</label>
+                <select className="border border-slate-300 p-2 rounded-lg w-full bg-white" id="category">
+                    {categories.map(category => (
+                        <option key={category.id} value={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="excercise" className="font-bold">Excercise:</label>
+                <input id="excercise" type="text" className="border border-slate-300 p-2 rounded-lg" placeholder="Ej. Food" />
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">Calories:</label>
+                <input id="calories" type="number" className="border border-slate-300 p-2 rounded-lg" placeholder="Calories" />
+            </div>
+
+            <input type="submit" className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" value="Save food or excercise" />
+        </form>
     )
 }
